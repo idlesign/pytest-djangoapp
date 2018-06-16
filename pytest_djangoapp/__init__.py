4a5a4dcac13a23bff1d96a39b9d4def234f643c4
idle sign <idlesign@yandex.ru>
2018-06-16 14:19:45 +0700

    Release v0.1.0

--- pytest_djangoapp/__init__.py
+++ pytest_djangoapp/__init__.py
@@ -4,9 +4,8 @@ from __future__ import unicode_literals
 from .toolbox import configure_djangoapp_plugin
 
 
-VERSION = (0, 0, 0)
+VERSION = (0, 1, 0)
 """Application version number tuple."""
 
 VERSION_STR = '.'.join(map(str, VERSION))
 """Application version number string."""
-
