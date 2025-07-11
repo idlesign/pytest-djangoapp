69afc276192444dc077a3b10a0c28436e85e90a6
idle sign <idlesign@yandex.ru>
2025-07-11 09:20:45 +0700

    Add 'run_app' fixture.

--- src/pytest_djangoapp/fixtures/utils.py
+++ src/pytest_djangoapp/fixtures/utils.py
@@ -1,6 +1,7 @@
 from time import time
 
 import pytest
+from django.utils import translation
 
 from ..configuration import Configuration
 
@@ -14,3 +15,43 @@ def get_stamp() -> str:
 def conf_app_name() -> str:
     """Returns your application name (deduced or provided)."""
     return Configuration.get_dict()[Configuration.KEY_APP]
+
+
+@pytest.fixture()
+def run_app(command_run, user_create, capsys, settings):
+    """Issues runserver command using current configuration
+    (dynamic or defined by user through `configure_djangoapp_plugin()`).
+
+    Useful for manual verification and debug, to have look and feel
+    of an enduser project with your app.
+
+    Run the test using the fixture as usual. It'll start Django development server.
+    Stop server with Ctrl+C.
+
+    Admin user is automatically created: username=admin password=admin
+
+    ```py
+    def test_me(run_app):
+        ...
+        run_app()
+    ```
+
+    """
+    def run_app_(*, username: str = 'admin', password: str = 'admin', **kwargs):
+        options = {
+            'use_reloader': False,
+        }
+
+        settings.DEBUG = True
+        settings.WSGI_APPLICATION = None  # override possibly provided by user in configure_djangoapp_plugin()
+
+        user_create(superuser=True, attributes={
+            'username': username,
+            'password': password,
+        })
+
+        with capsys.disabled():
+            command_run('runserver', options=options)
+
+    return run_app_
+
