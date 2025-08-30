598dd4562edeb970d706d69272481eaaf8b63b8a
idle sign <idlesign@yandex.ru>
2025-08-30 14:18:38 +0700

    Update test.

--- tests/test_etc.py
+++ tests/test_etc.py
@@ -1,17 +1,7 @@
-import sys
-
-import pytest
-
 from pytest_djangoapp import configure_djangoapp_plugin
 from pytest_djangoapp.configuration import Configuration
 
 
-@pytest.mark.skipif(sys.version_info > (2, 0), reason='teardown without setup should pass')
-def test_teardown_without_setup():
-    assert True
-
-
-@pytest.mark.skipif(sys.version_info < (3, 0), reason='whole project test are for Py 3')
 def test_whole_project_testing(request):
     configure_djangoapp_plugin('tests.settings_project')
     settings_dict = Configuration.get_combined(request.config)
