b9d0fc0bcf74043c053028af5eaa383e46320e96
idle sign <idlesign@yandex.ru>
2025-09-28 11:15:41 +0700

    Django 2.1 compat.

--- src/pytest_djangoapp/fixtures/signals.py
+++ src/pytest_djangoapp/fixtures/signals.py
@@ -21,7 +21,13 @@ from django.db.models.signals import (
     pre_save,
 )
 from django.test.signals import template_rendered
-from django.utils.autoreload import autoreload_started, file_changed
+from django.utils.autoreload import file_changed
+
+try:
+    from django.utils.autoreload import autoreload_started
+
+except ImportError:  # 2.1
+    autoreload_started = Signal()  # fake
 
 
 @dataclass(slots=True)
